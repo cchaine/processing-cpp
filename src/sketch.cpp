1f7f8a3c24f83b75742baa016f796f80e09cc47f
cchaine <cc.chaine@free.fr>
2018-05-05 09:41:51 +0200

    added element drawing

--- src/sketch.cpp
+++ src/sketch.cpp
@@ -49,19 +49,30 @@ void Sketch::run() {
         0.5f, 0.5f,
         -0.5f, 0.5f,
         -0.5f, -0.5f,
-        0.5f, 0.5f,
-        -0.5f, -0.5f,
         0.5f, -0.5f
     };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+
     GLuint vao;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
+
     GLuint vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+
+    GLuint ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
@@ -99,7 +110,7 @@ void Sketch::run() {
 
         glUseProgram(program);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
         glUseProgram(0);
 
